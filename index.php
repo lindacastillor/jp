13145be99eb1800bb11dbe0c4a8637de1a990660
lindacastillor <lindacastillor@gmail.com>
2023-04-02 18:17:27 -0600

    test

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<!-- silence is gold -->
+<!DOCTYPE html>
+<html lang="en" style="margin-top:0!important;">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<div class="agl agl-fadeCSSLeft">
+<header class="bg-yellow pt-100 pb-100">
+	<div class="container">
+		<nav class="row">
+			<div class="col-12">
+				<img src="../img/logo-javier.png" alt="" style="max-width:200px;">
+			</div>
+			<ul class="main-nav m-auto">
+				<li><a href="" class="current">pagina ejemplo</a></li>
+				<li><a href="">politica-privacidad</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+
+
+
+</div>
+</body>
+</html></html:>
